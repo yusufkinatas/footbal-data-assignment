81edeba36c53543ce9f5cb8be1d361e637f6fba6
Yusuf Kinatas <yusufkinatas@gmail.com>
2023-07-08 18:47:06 +0300

    solve CORS issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      destination: "https://api.football-data.org/v4/:path*",
+    },
+  ],
 };
 
 module.exports = nextConfig;
