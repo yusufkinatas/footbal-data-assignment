edcc13d4d3dcf19ecab116c482c1980d04f570d2
Yusuf Kinatas <yusufkinatas@gmail.com>
2023-07-08 18:46:48 +0300

    add ChakraUI provider & SWRConfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SWRConfig
+      value={{
+        fetcher: (url) =>
+          fetch(`/api${url}`, {
+            headers: {
+              "X-Auth-Token": "3f3bbf423ffa43288a918d5a957d93ce",
+            },
+          }).then((res) => res.json()),
+      }}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SWRConfig>
   );
 };
 
